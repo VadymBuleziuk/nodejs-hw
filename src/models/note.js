c3a4a5b845dfcf322e8957b8f347a533b1438b4f
Vadym Buleziuk <666buzya@gmail.com>
2026-08-22 15:15:54 +0200

    done

---
+++ src/models/note.js
@@ -0,0 +1,39 @@
+// src/models/student.js
+
+import { model, Schema } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
